08be75c9f8af0e55f6a4f934ec957a4908300643
ECUF <ecommerceultrafarma@gmail.com>
2026-07-08 10:58:57 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Para usar com Firebase, preencha abaixo e ative window.USE_FIREBASE = true.
+// Enquanto estiver false, o sistema roda em modo demonstração via localStorage.
+window.USE_FIREBASE = false;
+
+window.firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};

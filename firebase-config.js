8eda0663d7e2ffed41e17811f36d3faaadf64ee7
ECUF <ecommerceultrafarma@gmail.com>
2026-07-21 11:22:33 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
-// Para usar com Firebase, preencha abaixo e ative window.USE_FIREBASE = true.
-// Enquanto estiver false, o sistema roda em modo demonstração via localStorage.
-window.USE_FIREBASE = false;
+window.USE_FIREBASE = true;
 
-window.firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDcXi2sMi9hbiEKVNrbOF2kNOdx1iVGYmI",
+  authDomain: "salasdereuniao-8c61e.firebaseapp.com",
+  projectId: "salasdereuniao-8c61e",
+  storageBucket: "salasdereuniao-8c61e.firebasestorage.app",
+  messagingSenderId: "971023893263",
+  appId: "1:971023893263:web:9304c007ae32d59a32b47a"
 };
